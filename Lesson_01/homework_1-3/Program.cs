b23d40f930bac95ee21230ec70a1aea87e4bd691
Elena <lena.kostyuk@gmail.com>
2022-08-02 18:38:20 +0300

    home task 1-3

---
+++ Lesson_01/homework_1-3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a < b)
+{
+    a = b;
+}
+if (a < c)
+{
+a = c;
+}
+
+Console.WriteLine($"The largest number is {a}");
