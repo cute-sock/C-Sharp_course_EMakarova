a51c6ed44ed96fa2824757a65ff2dc3c159ce5b9
Elena <lena.kostyuk@gmail.com>
2022-08-02 19:00:06 +0300

    home task 2-3

---
+++ Lesson_02/HW_2-3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+//void ThirdDigit(int num);
+//Console.WriteLine("Enter number: ");
+//{
+//int num = int.Parse(Console.ReadLine());
+//if (num <=99)
+//{
+//    Console.WriteLine("error! No third digit");
+//        return;
+//}
+///while (num >= 999)
+//Console.WriteLine(num % 10);
+//}
+//    
+//ThirdDigit(int.Parse(Console.ReadLine()));
+
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 99)
+    {
+        Console.WriteLine("there is no third digit");
+        return;
+    }
+
+    while (num > 999) num = num / 10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()));
