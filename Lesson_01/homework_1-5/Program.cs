95e568d69c29c21fdf645524ad4b5c01321e452e
Elena <lena.kostyuk@gmail.com>
2022-08-02 18:41:09 +0300

    home task 1-5

---
+++ Lesson_01/homework_1-5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter number: ");
+int n = int.Parse(Console.ReadLine());
+int index = 2;
+
+while (index <= n)
+{
+    Console.WriteLine($"{index} ");
+    index = index + 2;
+}
