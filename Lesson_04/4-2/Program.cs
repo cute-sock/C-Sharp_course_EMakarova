1df3a5b82e5e07577b7712a284e22ef7d771a3e1
Elena <lena.kostyuk@gmail.com>
2022-08-03 18:56:14 +0300

    task 4-2

---
+++ Lesson_04/4-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int SumMult(int num)
+{
+    int all_mult = 1;
+    for (int i = 1; i <= num; i++)
+        all_mult = all_mult * i;
+    return all_mult;
+}
+
+Console.WriteLine(SumMult(5));
