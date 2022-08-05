7ef7afc97bf99f15b08cad8e514fca8298b38deb
Elena <lena.kostyuk@gmail.com>
2022-08-05 11:46:00 +0300

    task 5-2

--- Lesson_05/5-2/Program.cs
+++ Lesson_05/5-2/Program.cs
@@ -13,20 +13,36 @@ void Print(int[] arr)
     Console.WriteLine();
 }
 
-int[] MassNum(int size, int from, int to)
+int[] MassNums(int size)
 {
     int[] arr = new int[size];
 
     for (int i = 0; i < size; i++)
     {
-        arr[i] = new
-        Random().Next(from, to);
+        arr[i] = new Random().Next(-9, 10);
     }
     return arr;
 }
 
-
-int SearchNum(int[] arr)
-{
-for(int i=0; i < arr.Length; i++)
+void IfInNum(int[] arr, int num)
+{   
+    string text = "No";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (num == arr[i])
+        {
+            text = "Yes";
+            break;
+        }
+    }
+    Console.WriteLine($"The num value {num} -> {text}");    
 }
+
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+IfInNum(arr_1, 4);
+
+int[] arr_2 = MassNums(12);
+Print(arr_2);
+IfInNum(arr_2, 7);
