eb19aadbe6ff7bb58264671d9f0ff95b0d5de9b7
Elena <lena.kostyuk@gmail.com>
2022-08-05 11:07:29 +0300

    task 5-2 to complete with trainer's version

---
+++ Lesson_05/5-2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNum(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new
+        Random().Next(from, to);
+    }
+    return arr;
+}
+
+
+int SearchNum(int[] arr)
+{
+for(int i=0; i < arr.Length; i++)
+}
