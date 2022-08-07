37d322d8b6adc17a20b8c2f39167ca0dee145b25
Elena <lena.kostyuk@gmail.com>
2022-08-07 15:27:47 +0300

    home task 4-1

---
+++ Lesson_04/HW_4-1/Program.cs
@@ -0,0 +1,21 @@
+﻿// ДЗ 1. Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+int Deg(int a, int b)
+{
+    int n_deg = a;
+
+    for (int i = 2; i <= b; i = i + 1)
+    {
+        n_deg *= a;
+        // n_deg = (Math.Pow(a, b)); - не работает...
+    }
+    return n_deg;
+}
+
+Console.WriteLine("Insert 1st digit: ");
+int num_a = int.Parse(Console.ReadLine());
+Console.WriteLine("Insert 2nd digit: ");
+int num_b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Deg(num_a, num_b));
