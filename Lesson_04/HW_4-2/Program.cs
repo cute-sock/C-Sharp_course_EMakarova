6385ddc1352067cad0b8d57c00b3d46290071da9
Elena <lena.kostyuk@gmail.com>
2022-08-07 16:21:54 +0300

    home task 4-2 to be corrected after checking

---
+++ Lesson_04/HW_4-2/Program.cs
@@ -0,0 +1,21 @@
+﻿// ДЗ 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sum_2_digits(int num)
+{
+    int summa = 0;
+
+    for (int i = num; num != 0; num /= 10)
+        
+        summa = num + num %10;
+        // sum += num % 10;
+
+    return summa;
+}
+
+
+Console.WriteLine("Insert number: ");
+int summa = int.Parse(Console.ReadLine());
+
+Console.WriteLine(sum_2_digits(summa));
+
+// NB: почему-то при сложении выдает на единицу меньше суммы...
