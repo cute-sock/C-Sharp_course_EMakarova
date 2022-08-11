2a05e3541c8839f9572a42619ac64329c28ddc90
Elena <lena.kostyuk@gmail.com>
2022-08-11 15:09:55 +0300

    home task 5-1

---
+++ Lesson_05/HW_5-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = new int[5];
+int x = 0, sum = 0;
+Random rnd = new Random();
+
+Console.WriteLine();
+for (int i = 0; i < 5; i++)
+{
+    arr[i] = rnd.Next(100, 999);
+    Console.Write("{0} ", arr[i]);
+}
+Console.WriteLine();
+
+foreach (int element in arr)
+{
+    Console.Write("{0} ", element);
+    if (element % 2 == 0)
+    {
+        x++;
+        sum += element;
+    }
+}
+
+Console.WriteLine("even numbers -> {0} ", x, sum);
+
+
