6bc713f8305960bf58a86cb190e12d3d5eb44984
Elena <lena.kostyuk@gmail.com>
2022-08-12 11:16:33 +0300

    task 7-4

--- Lesson_07/7-1/Program.cs
+++ Lesson_07/7-1/Program.cs
@@ -8,40 +8,5 @@
 
 // строка (i) начинается с нуля, а столбец (j) - с единицы
 
-void Print(int[,] arr)
-{
-    int row_size = arr.GetLength(0);
-    int column_size = arr.GetLength(1);
-
-    for(int i = 0; i < row_size; i++)
-    {
-        for(int j = 0; j < column_size; j++)
-        {
-            Console.Write($" {arr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-int [,] MussNums (int row, int column)
-{
-    int [,] arr = new int[row, column];
-
-    for (int i=0; i< row; i++)
-    {
-        for (int j=0; j< column; j++)
-        {
-            arr[i, j] = i+j;
-        }
-    }
-    return arr;
-}
-
-
-Console.Write("Enter the number of rows: ");
-int row = int.Parse(Console.ReadLine());
-Console.Write("Enter the number of columns: ");
-int column = int.Parse(Console.ReadLine());
 
 
