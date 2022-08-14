70f0f9628fbde3dd0b58dec351efdb7c3ccfcfd7
Elena <lena.kostyuk@gmail.com>
2022-08-14 22:53:53 +0300

    home task 6-1

---
+++ Lesson_06/HW_6-2/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// ДЗ-2 - методичка для решения https://studok.net/obrazovanie-i-kommunikacii/vopros-kak-vychislit-tochku-peresechenija-dvuh-prjamyh/
+// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 *x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 =
+// 9 -> (-0,5; -0,5)
