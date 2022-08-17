e0b95260090158bbbc2830917edd70cb9cb94be1
Elena <lena.kostyuk@gmail.com>
2022-08-17 14:25:29 +0300

    tasks upd before homework

---
+++ Lesson_08/HW_8-3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 3: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// https://blog.fenix.help/podgotovka-k-testam-yekzamenam-zachetam/pravilo-umnozheniya-matrits-primery-s-resheniyem
+// https://lfirmal.com/matritsyi-chastnyie-vidyi-matrits/#%D0%A3%D0%BC%D0%BD%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86
+
