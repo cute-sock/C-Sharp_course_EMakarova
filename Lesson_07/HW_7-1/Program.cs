bd35417d43a737032c0921290a70b1fb086bbeaf
Elena <lena.kostyuk@gmail.com>
2022-08-18 21:19:58 +0300

    homework 7-1

--- Lesson_07/HW_7-1/Program.cs
+++ Lesson_07/HW_7-1/Program.cs
@@ -5,3 +5,41 @@
 // 8 7,8 -7,1 9
 
 // Console.Write($" {arr[i, j], 6} "); - это как сделать ровненько
+
+
+Console.Write("Enter number of rows (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[m, n];
+
+NewArray(arr);
+
+WriteArray(arr);
+
+Console.WriteLine();
+
+void NewArray(double[,] arr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().NextDouble() * 100 - 50;
+        }
+    }
+}
+
+void WriteArray(double[,] arr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(arr[i, j], 1);
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
