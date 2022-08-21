9a84c81e9a01a6d5d9b9a70a933a17afcddff5c8
Elena <lena.kostyuk@gmail.com>
2022-08-21 21:09:55 +0300

    HW_7-3 corrected after checking

--- Lesson_07/HW_7-3/Program.cs
+++ Lesson_07/HW_7-3/Program.cs
@@ -16,9 +16,7 @@ void Print(int[,] arr)
     for (int i = 0; i < row_size; i++)
     {
         for (int j = 0; j < column_size; j++)
-        {
             Console.Write($" {arr[i, j]} ");
-        }
         Console.WriteLine();
     }
     Console.WriteLine();
@@ -29,40 +27,31 @@ int[,] MassNums(int row, int column, int from, int to)
     int[,] arr = new int[row, column];
 
     for (int i = 0; i < row; i++)
-    {
         for (int j = 0; j < column; j++)
-        {
             arr[i, j] = new Random().Next(from, to);
-        }
-    }
     return arr;
 }
 
-void AvgSumColumns(int[,] arr)
+void ArithmeticMean(int[,] arr)
 {
     int row = arr.GetLength(0);
     int column = arr.GetLength(1);
+    double res;
 
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < column; i++)
     {
-        double arithmeticMean = 0;
-
-        for (int j = 0; j < column; j++)
-        {
-            arithmeticMean += arr.GetLength(0);
-        }
-
-        arithmeticMean = Math.Round(arithmeticMean / column, 1);
-        Console.WriteLine(AvgSumColumns($"Column # {i + 1} {arithmeticMean}"));
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
     }
 }
 
-Console.Write("Enter number of rows: ");
-int.Parse(Console.ReadLine());
-Console.Write("Enter number of columns: ");
-int.Parse(Console.ReadLine());
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
 
-int[,] arr_1 = MassNums(row, column, 1, 10);
+int[,] arr_1 = MassNums(row, column, 1, 6);
 Print(arr_1);
 
-Console.WriteLine(AvgSumColumns(arr_1));
+ArithmeticMean(arr_1);
