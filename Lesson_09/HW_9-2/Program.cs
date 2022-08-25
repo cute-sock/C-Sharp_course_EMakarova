c28f7d90c80672d54fc8892407ffdd89ea66743d
Elena <lena.kostyuk@gmail.com>
2022-08-25 18:19:54 +0300

    homework 9-1, не получилось вывести только сумму без вывода чисел в диапазоне

---
+++ Lesson_09/HW_9-2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+// https://youtu.be/rh1mP02NFoM?t=229
+// https://skillbox.ru/media/code/rekursiya-vokrug-nas/
+//https://docs.microsoft.com/ru-ru/archive/msdn-magazine/2012/december/csharp-matrix-decomposition
+
+
+int SumMN(int m, int n, int sum)
+{
+
+    while (m <= n)
+    {
+        Console.Write(m + " ");
+        sum += m;
+        m++;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumMN(1, 3, 0));
